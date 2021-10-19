dd74f054f4c29ae2ce6c06f2447b0bb9d854818a
Victor Luiz <luizvic.2112@gmail.com>
2021-10-18 21:23:54 -0300

    feat: programming improvementscreate

--- server/src/repositories/prisma/PrismaItensRepository.ts
+++ server/src/repositories/prisma/PrismaItensRepository.ts
@@ -4,32 +4,44 @@ import { IItensRepositories } from '@repository/IItensRepositories'
 
 class PrismaItensRepository implements IItensRepositories {
   async findById (id: number): Promise<Item> {
-    const item = await prisma.item.findUnique({
-      where: {
-        id
-      }
-    })
+    try {
+      const item = await prisma.item.findUnique({
+        where: {
+          id
+        }
+      })
 
-    return item
+      return item
+    } finally {
+      await prisma.$disconnect()
+    }
   }
 
   async findAll (): Promise<Item[]> {
-    const itens = await prisma.item.findMany()
-    return itens
+    try {
+      const itens = await prisma.item.findMany()
+      return itens
+    } finally {
+      await prisma.$disconnect()
+    }
   }
 
   async saveAll (itens: Item[]): Promise<void> {
-    for (const item of itens) {
-      await prisma.item.upsert({
-        create: item,
-        update: {
-          title: item.title,
-          image: item.image
-        },
-        where: {
-          id: item.id
-        }
-      })
+    try {
+      for (const item of itens) {
+        await prisma.item.upsert({
+          create: item,
+          update: {
+            title: item.title,
+            image: item.image
+          },
+          where: {
+            id: item.id
+          }
+        })
+      }
+    } finally {
+      await prisma.$disconnect()
     }
   }
 }
